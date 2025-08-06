f18ff848777a33d3c6c2782b91b31f25fa566155
krish <krish434shharma@gmail.com>
2025-08-06 10:51:02 +0530

    all the given  tasks completed successfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    Product p = new Product();
 
+    int x = 10;
+    int y = 9;
+    int z = 8;
+    double a = 7.5;
+    double b = 3.4;
+
+    int task1 = p.product(x,y);
+    int task2 = p.product(x,y,z);
+    double task3 = p.product(a,b);
 }
